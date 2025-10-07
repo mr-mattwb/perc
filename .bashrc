1c2f5316706150c210ed8a5e99cac513a939bb59
Matt Boyd <mr.mattwb@gmail.com>
2025-10-07 08:50:13 -0400

    Got a working lisp log parser.  Need to try it on ndf.log

--- .bashrc
+++ .bashrc
@@ -13,3 +13,4 @@ export PATH=$HOME/mine/perc:$PATH
 echo initialized
 
 alias ctop='ledit ./ccaml -I +unix -I +str -I .'
+alias lisp='lisp --load .lisp'
--- plog.lisp
+++ plog.lisp
@@ -5,8 +5,8 @@
 
 (defvar pfx-date "([0-9][0-9][0-9][0-9]\-[0-9][0-9]\-[0-9][0-9])")
 
-(defmethod date-of-ints (year month day) (make-instance 'date :date (+ (* 10000 year) (* 100 month) day)))
-(defmethod date-of-strs (year month day) (date-of-ints (parse-integer year) (parse-integer month) (parse-integer day)))
+(defmethod make-date (year month day) (make-instance 'date :date (+ (* 10000 year) (* 100 month) day)))
+(defmethod date-of-strs (year month day) (make-date (parse-integer year) (parse-integer month) (parse-integer day)))
 (defmethod date-of-string (ymd) (date-of-strs (subseq ymd 0 4) (subseq ymd 5 7) (subseq ymd 8 10)))
 
 (defmethod year ((d date)) (floor (/ (date d) 10000)))
@@ -16,24 +16,103 @@
 (defmethod now-date ()
     (multiple-value-bind (a b c mday mon yr d e f) 
         (get-decoded-time) 
-        (date-of-ints yr mon mday)))
+        (make-date yr mon mday)))
 
-(defclass mtime () ((mtime 
-                    :accessor mtime 
-                    :initarg :mtime)))
+(defun four-digit-string (s) 
+  (let ((pfx (if (< s 10) "000" (if (< s 100) "00" (if (< s 1000) "0" ""))))) 
+    (format nil "~A~A" pfx s)))
+(defun two-digit-string (s) 
+  (let ((pfx (if (< s 10 ) "0" "")))
+    (format nil "~A~A" pfx s)))
+(defmethod to-string ((d date)) 
+  (format t "~A-~A-~A" (four-digit-string (year d)) (two-digit-string (month d)) (two-digit-string (day d))))
+  
+(defclass ttime () ((ttime 
+                    :accessor ttime 
+                    :initarg :ttime)))
 
-(defvar pfx-time "([0-9][0-9]:[0-9][0-9]:[0-9][0-9])")
+(defvar pfx-time "([0-9][0-9]:[0-9][0-9]:[0-9][0-9]),([0-9]+)")
 
-(defmethod time-of-ints (hr mn sc) (make-instance 'mtime :mtime (+ (* 3600 hr) (* 60 mn) sc)))
-(defmethod time-of-strs (hr mn sc) (time-of-ints (parse-integer hr) (parse-integer mn) (parse-integer sc)))
+(defmethod make-time (hr mn sc) (make-instance 'ttime :ttime (+ (* 3600 hr) (* 60 mn) sc)))
+(defmethod time-of-strs (hr mn sc) (make-time (parse-integer hr) (parse-integer mn) (parse-integer sc)))
 (defmethod time-of-string (hms) (time-of-strs (subseq hms 0 2) (subseq hms 3 5) (subseq hms 6 8)))
 
-(defmethod mhour ((tm mtime)) (floor (/ (mtime tm) 3600)))
-(defmethod mminute ((tm mtime)) (mod (floor (/ (mtime tm) 60)) 60))
-(defmethod msecond ((tm mtime)) (mod (mtime tm) 60))
+(defmethod thour ((tm ttime)) (floor (/ (ttime tm) 3600)))
+(defmethod tminute ((tm ttime)) (mod (floor (/ (ttime tm) 60)) 60))
+(defmethod tsecond ((tm ttime)) (mod (ttime tm) 60))
+
+(defmethod to-string ((tm ttime))
+    (format t "~A:~A:~A" (two-digit-string (thour tm)) (two-digit-string (tminute tm)) (two-digit-string (tsecond tm))))
 
 (defmethod now-time ()
     (multiple-value-bind (sc mn hr a b c d e f)
         (get-decoded-time)
-        (time-of-ints hr mn sc)))
+        (make-time hr mn sc)))
+
+(defclass datetime (date ttime) ())
+
+(defmethod to-string ((dt datetime)) 
+    (format t "~A-~A-~A ~A:~A:~A"  
+        (four-digit-string (year dt)) (two-digit-string (month dt)) (two-digit-string (day dt))
+        (two-digit-string (thour dt)) (two-digit-string (tminute dt)) (two-digit-string (tsecond dt))))
+
+(defmethod make-datetime (yr mo dy hr mi sc) 
+    (make-instance 'datetime 
+                   :date (+ (* yr 10000) (* mo 100) dy)
+                   :ttime (+ (* 3600 hr) (* 60 mi) sc)))
+
+(defmethod now ()
+  (multiple-value-bind (sc mn hr dy mo yr a b c)
+    (get-decoded-time)
+    (make-datetime yr mo dy hr mn sc)))
+
+(defvar pfx-callid "\\|([0-9A-F]*)")
+(defvar pfx-ivr "\\|([_0-9A-Z\.\-]*)")
+
+(defvar chain-str "\\|reporting\.CDRUtil\\|chaining from >(.*)< to >(.*)<")
+
+(defvar chain-pfx (concatenate 'string pfx-date "T" pfx-time pfx-callid pfx-ivr))
+(defvar chaining  (concatenate 'string chain-pfx chain-str))
+
+(defvar chainline "2025-08-25T05:24:35,593|6CF7AC02C98898345960E7A47D41C6E1|MOD25.09.0.004-DEBUG|reporting.CDRUtil|chaining from >contr0105_CheckNodeCount_DS< to >contr0110_CheckGlobalHandling_DS<")
+
+(defclass entry ()
+  ((tdate :accessor tdate :initarg :tdate)
+   (ttime :accessor ttime :initarg :ttime)
+   (tmsec :accessor tmsec :initarg :tmsec)
+   (id :accessor id :initarg :id)
+   (ivr :accessor ivr :initarg :ivr)))
+
+(defclass chain-entry (entry)
+    ((tfrom :accessor tfrom :initarg :tfrom)
+     (tto :accessor tto :initarg :tto)))
+  
+(defmethod parse-line (line)
+  (ppcre:register-groups-bind (td tt tm tid tiv ttf ttt) (chaining line)
+    (make-instance 'chain-entry 
+        :tdate (date-of-string td)
+        :ttime (time-of-string tt)
+        :tmsec (parse-integer tm) 
+        :id tid 
+        :ivr tiv
+        :tfrom ttf
+        :tto ttt)))
+
+(defmethod parse-file (fname) 
+    (with-open-file (fin fname)
+      (labels 
+        ((looplines (lines) 
+            (let ((line (read-line fin nil 'eof)))
+              (cond
+                ((eq line 'eof) (reverse lines))
+                ((cl-ppcre:scan chaining line) (looplines (cons (parse-line line) lines)))
+                (t (looplines lines))))))
+        (looplines nil))))
+
+
+
+
+
+
+
 
